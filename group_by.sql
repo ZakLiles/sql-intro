c1d35403a95234f7dce7fa0beec63004c0937fc1
Zak Liles <zak.liles@gmail.com>
2022-01-16 19:05:48 -0700

    Completed Group By Section

---
+++ group_by.sql
@@ -0,0 +1,16 @@
+--1
+SELECT SUM(total), billing_state
+FROM invoice
+GROUP BY billing_state;
+
+--2
+SELECT AVG(milliseconds), album_id
+FROM track
+GROUP BY album_id
+ORDER BY AVG(milliseconds);
+
+--3
+SELECT COUNT(*), artist_id
+FROM album
+WHERE artist_id IN(8,22)
+GROUP BY (artist_id);
